b6ad57a2cc2c05c1da0cef79079ce695d98a1a20
Erik Rodriguez <eric19.21.03@gmail.com>
2020-07-29 08:36:09 -0700

    Adds cell box layout for grid.js

--- game-of-life/src/components/Grid.js
+++ game-of-life/src/components/Grid.js
@@ -1,20 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
+import "../App.css";
 
 const Grid = () => {
   function createGrid(num) {
+    // create columns array that holds all other arrays
     const gridCol = [];
 
-    for (let i = 0; i < gridCol.length; i++) {
-      gridCol[i] = new Array(gridRows);
+    for (let i = 0; i < num; i++) {
+      // create rows with num length and push into gridCol
+      const gridRows = [];
+
+      for (let j = 0; j < num; j++) {
+        gridRows.push(0);
+      }
+
+      // each row is added once filled
+      gridCol.push(gridRows);
     }
 
     return gridCol;
   }
 
-  // initialize use state with a 25 by 25 grid
-  const [grid, setGrid] = useState(true);
+  // initialize state with a 25 by 25 grid
+  const grid = createGrid(10);
 
-  return <div>Hello test</div>;
+  return <div className="theGrid">
+    {/* box design for cells to create grid look */}{
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid.length; j++) {
+      if (grid[i][j] == 1) {
+        return <div className='gridCell' style={{background: "red"}}/>
+      }
+      <div className='gridCell'/>
+    }
+  }}
+  </div>;
 };
 
 export default Grid;
