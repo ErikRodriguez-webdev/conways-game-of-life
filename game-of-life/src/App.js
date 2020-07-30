03f54b332ad1397bc0a1273a7942284d2a6e1296
Erik Rodriguez <eric19.21.03@gmail.com>
2020-07-30 08:37:29 -0700

    Adds styling to game speed buttons and Adds some comments

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -3,9 +3,18 @@ import produce from "immer";
 import "./App.css";
 
 function App() {
+  // square graph rows and cols
   const numRows = 25;
   const numCols = 25;
 
+  // set timeout ms count used for speed
+  const slow = 1250;
+  const medium = 500;
+  const fast = 0;
+
+  let [gameSpeed, setGameSpeed] = useState(medium);
+
+  // used to traverse around and count neighbors
   const operations = [
     [0, 1],
     [0, -1],
@@ -17,17 +26,20 @@ function App() {
     [-1, 0],
   ];
 
-  const generateEmptyGrid = () => {
+  const createEmptyGrid = () => {
+    // create row main array
     const rows = [];
+    // for loop through num of rows
     for (let i = 0; i < numRows; i++) {
+      // push in an empty array that holds num or col of zeroes
       rows.push(Array.from(Array(numCols), () => 0));
     }
-
+    // return rows grid structure
     return rows;
   };
 
   const [grid, setGrid] = useState(() => {
-    return generateEmptyGrid();
+    return createEmptyGrid();
   });
 
   const [running, setRunning] = useState(false);
@@ -47,29 +59,29 @@ function App() {
 
     setGeneration(genRef.current + 1);
 
-    setGrid((g) => {
-      return produce(g, (gridCopy) => {
+    setGrid((lastGen) => {
+      return produce(lastGen, (gridCopy) => {
         for (let i = 0; i < numRows; i++) {
           for (let j = 0; j < numCols; j++) {
-            let neighbors = 0;
+            let countNeighbors = 0;
             operations.forEach(([x, y]) => {
               const newI = i + x;
               const newJ = j + y;
               if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
-                neighbors += g[newI][newJ];
+                countNeighbors += lastGen[newI][newJ];
               }
             });
 
-            if (neighbors < 2 || neighbors > 3) {
+            if (countNeighbors < 2 || countNeighbors > 3) {
               gridCopy[i][j] = 0;
-            } else if (g[i][j] === 0 && neighbors === 3) {
+            } else if (lastGen[i][j] === 0 && countNeighbors === 3) {
               gridCopy[i][j] = 1;
             }
           }
         }
       });
     }, []);
-    setTimeout(runSimulation, 1000);
+    setTimeout(runSimulation, gameSpeed);
   }, []);
 
   return (
@@ -104,12 +116,18 @@ function App() {
         </button>
         <button
           onClick={() => {
-            setGrid(generateEmptyGrid());
+            setGrid(createEmptyGrid());
             setGeneration(0);
           }}
         >
           Clear Grid
         </button>
+        <div className="gameSpeed">
+          <div>{`Game Speed: ${gameSpeed} ms`}</div>
+          <button onClick={() => setGameSpeed(slow)}>slow</button>
+          <button onClick={() => setGameSpeed(medium)}>medium</button>
+          <button onClick={() => setGameSpeed(fast)}>fast</button>
+        </div>
       </div>
       <div
         style={{
@@ -130,8 +148,8 @@ function App() {
               style={{
                 width: 20,
                 height: 20,
-                backgroundColor: grid[i][j] ? "white" : null,
-                border: "solid 1px black",
+                backgroundColor: grid[i][j] ? "white" : "black",
+                border: "solid 1px silver",
               }}
             />
           ))
