4b31836dc1e17eaa6a4863f29dcd2920166809f6
Erik Rodriguez <eric19.21.03@gmail.com>
2020-07-29 21:06:27 -0700

    Adds new text to game and Adds generations to display on app.js

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useCallback, useRef } from "react";
+import produce from "immer";
 import "./App.css";
 
 function App() {
-  const numRows = 50;
-  const numCols = 50;
+  const numRows = 25;
+  const numCols = 25;
 
   const operations = [
     [0, 1],
@@ -34,6 +35,8 @@ function App() {
   const runningRef = useRef(running);
   runningRef.current = running;
 
+  var genCount = 0;
+
   const runSimulation = useCallback(() => {
     if (!runningRef.current) {
       return;
@@ -42,90 +45,93 @@ function App() {
     setGrid((g) => {
       return produce(g, (gridCopy) => {
         for (let i = 0; i < numRows; i++) {
-          for (let k = 0; k < numCols; k++) {
+          for (let j = 0; j < numCols; j++) {
             let neighbors = 0;
             operations.forEach(([x, y]) => {
               const newI = i + x;
-              const newK = k + y;
-              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
-                neighbors += g[newI][newK];
+              const newJ = j + y;
+              if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+                neighbors += g[newI][newJ];
               }
             });
 
             if (neighbors < 2 || neighbors > 3) {
-              gridCopy[i][k] = 0;
-            } else if (g[i][k] === 0 && neighbors === 3) {
-              gridCopy[i][k] = 1;
+              gridCopy[i][j] = 0;
+            } else if (g[i][j] === 0 && neighbors === 3) {
+              gridCopy[i][j] = 1;
             }
           }
         }
+        genCount = genCount + 1;
       });
-    });
-
-    setTimeout(runSimulation, 100);
+    }, []);
+    setTimeout(runSimulation, 1000);
   }, []);
 
   return (
-    <>
-      <button
-        onClick={() => {
-          setRunning(!running);
-          if (!running) {
-            runningRef.current = true;
-            runSimulation();
-          }
-        }}
-      >
-        {running ? "stop" : "start"}
-      </button>
-      <button
-        onClick={() => {
-          const rows = [];
-          for (let i = 0; i < numRows; i++) {
-            rows.push(
-              Array.from(Array(numCols), () => (Math.random() > 0.7 ? 1 : 0))
-            );
-          }
+    <div className="game">
+      <div className="gameButtons">
+        <div style={{ color: "white" }}>{`Generation: ${genCount}`}</div>
+        <button
+          onClick={() => {
+            setRunning(!running);
+            if (!running) {
+              runningRef.current = true;
+              runSimulation();
+            }
+          }}
+        >
+          {running ? "Stop Game" : "Start Game"}
+        </button>
+        <button
+          onClick={() => {
+            const rows = [];
+            for (let i = 0; i < numRows; i++) {
+              rows.push(
+                Array.from(Array(numCols), () => (Math.random() > 0.7 ? 1 : 0))
+              );
+            }
 
-          setGrid(rows);
-        }}
-      >
-        random
-      </button>
-      <button
-        onClick={() => {
-          setGrid(generateEmptyGrid());
-        }}
-      >
-        clear
-      </button>
+            setGrid(rows);
+          }}
+        >
+          Randomize Grid
+        </button>
+        <button
+          onClick={() => {
+            setGrid(generateEmptyGrid());
+          }}
+        >
+          Clear Grid
+        </button>
+      </div>
       <div
         style={{
           display: "grid",
-          gridTemplateColumns: `repeat(${numCols}, 20px)`,
+          gridTemplateColumns: `repeat(${numCols}, 23px)`,
         }}
       >
         {grid.map((rows, i) =>
-          rows.map((col, k) => (
+          rows.map((col, j) => (
             <div
-              key={`${i}-${k}`}
+              key={`${i}-${j}`}
               onClick={() => {
                 const newGrid = produce(grid, (gridCopy) => {
-                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                  gridCopy[i][j] = grid[i][j] ? 0 : 1;
                 });
                 setGrid(newGrid);
               }}
               style={{
                 width: 20,
                 height: 20,
-                backgroundColor: grid[i][k] ? "pink" : undefined,
+                backgroundColor: grid[i][j] ? "white" : null,
                 border: "solid 1px black",
               }}
             />
           ))
         )}
       </div>
-    </>
+    </div>
   );
 }
 
