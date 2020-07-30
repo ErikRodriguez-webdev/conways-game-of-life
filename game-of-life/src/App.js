aa8f1cb38091fd23e51f2a8da24601f02b997ed8
Erik Rodriguez <eric19.21.03@gmail.com>
2020-07-29 20:21:27 -0700

    Adds dependencies and creates empty grid function

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useCallback, useRef } from "react";
 import "./App.css";
 
 function App() {
+  const numRows = 50;
+  const numCols = 50;
+
+  const operations = [
+    [0, 1],
+    [0, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+    [-1, -1],
+    [1, 0],
+    [-1, 0],
+  ];
+
+  const generateEmptyGrid = () => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+
+    return rows;
+  };
+
   return <div className="App">hello</div>;
 }
 
