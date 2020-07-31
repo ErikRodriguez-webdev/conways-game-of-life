e568078c8cc1c8ac5fca6b5555994056d4365c33
Erik Rodriguez <eric19.21.03@gmail.com>
2020-07-31 15:09:52 -0700

    Adds about the algo in info.js

--- game-of-life/src/components/Info.js
+++ game-of-life/src/components/Info.js
@@ -5,9 +5,6 @@ const Info = () => {
   return (
     <div className="infoPage">
       <p>Rules:</p>
-      <p>Alive is 1 or white</p>
-      <p>Dead is 0 or black</p>
-
       <ol style={{ textAlign: "left" }}>
         <li> Any live cell with two or three live neighbours survives.</li>
         <li> Any dead cell with three live neighbours becomes a live cell.</li>
@@ -25,6 +22,14 @@ const Info = () => {
         which this happens is sometimes called a tick. Each generation is a pure
         function of the preceding one. The rules continue to be applied
         repeatedly to create further generations until death.
+      </p>
+      <p>
+        A square from the grid is a cell. If that cell is alive it is
+        represented with the color white or 1. However, if that cell is dead it
+        is represented with the color black or 0. That cell will die if the
+        surrounding neighbor count is one or less, due to under population.
+        Similarly if the surrounding cells are four or more then the cell will
+        die, due to over population.
       </p>
     </div>
   );
